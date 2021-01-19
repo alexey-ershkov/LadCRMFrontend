5a361bdf05543da491afc22f8b4988dbf1369e6c
alexey ershkov <ershkov.alex2000@gmail.com>
2021-01-19 21:50:17 +0300

    client card done

---
+++ src/includes/clientCard/clientCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import './clientCard.scss';
+import Client from "../../models/client";
+import { Link } from "react-router-dom";
+
+interface IClientCardProps {
+    client: Client
+}
+
+function ClientCard({client}:IClientCardProps):JSX.Element {
+
+    return (<Link to={`/client/${client._id}`} className={'clientCard'}>
+        <div className={'clientNames'}>
+            {client.surname} {client.name}  {client.lastName}
+        </div>
+        <div className={'clientInfo'}>
+            <div className={'phone'}>
+                <p className={'title'}>Телефон: </p> &nbsp; {client.phone}
+            </div>
+            <div className={'orderNumber'}>
+                <p className={'title'}>Номер договора:</p> &nbsp; {client.orderNumber}
+            </div>
+        </div>
+    </Link>)
+}
+
+export default ClientCard;
--- src/pages/main/MainPage.tsx
+++ src/pages/main/MainPage.tsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './MainPage.scss'
 import SearchBar from "./searchBar/SearchBar";
+import ClientCard from "../../includes/clientCard/clientCard";
+import getClients from "../../api/client/getClients";
+import Client from "../../models/client";
+import PulseLoader from "react-spinners/PulseLoader";
+import consts from "../../consts";
+
 
 function MainPage(): JSX.Element {
+
+    const [clients, setClients] = useState([])
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        getClients().then(
+            data => {
+                setClients(data)
+                setLoading(false);
+            }
+        )
+    }, [])
+
+    if (isLoading) {
+        return (
+            <div>
+                <SearchBar/>
+                <div className={'loadIndicator'}>
+                    <PulseLoader color={consts.ACCENT_COLOR_HEX} loading={true}/>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div>
             <SearchBar/>
+            <div className={'clientsContainer'}>
+                {clients.map((value:Client) => {
+                    return <ClientCard client={value} key={value._id}/>
+                })}
+            </div>
         </div>
     )
 }
